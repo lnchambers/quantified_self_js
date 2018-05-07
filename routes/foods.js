17d9eb8b5ebe5241da8b17d1e71fd5075dbe46e3
Luke Chambers <lukenatchambers@gmail.com>
2018-05-07 09:14:33 -0600

    Add gitignore

---
+++ routes/foods.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send({ some: 'json' })
+})
+
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id
+  res.send({ specific: 'json' })
+})
+
+router.post()
+
+module.exports = router;
